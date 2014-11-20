df6fea0811c4e265bcd6415b357cff1cfcb57f75
Conor Smyth <you@example.com>
2014-11-20 10:34:39 +0000

    commented out stuff

--- hotel.c
+++ hotel.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 #define SLEEP 10
 #define MAXROOM 1000
 
 int main() {
 	while(1) {
-		int roomNumber = randomRoom();
+		//int roomNumber = randomRoom();
 		//Registering: RoomNumber Day Month Date Time Year
-		printf("Registering: %d\n", roomNumber);
+		//printf("Registering: %d\n", roomNumber);
 	}
 }
