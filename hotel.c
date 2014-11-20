7601f02aa3cd1a8aecdf7daa56b6354f1fff468a
Conor Smyth <cnrsmyth@gmail.com>
2014-11-20 20:04:06 +0000

    Started random number generating. SEGFAULT :'(

--- hotel.c
+++ hotel.c
@@ -10,13 +10,75 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#define SLEEP 10
-#define MAXROOM 1000
+#define SLEEP 5
+#define MAXROOM 5000
+
+typedef struct {
+	int generated;
+	int pending;
+	int called;
+	int expired;
+} logs_t;
+
+typedef struct {
+	int roomNumber;
+	int callTime;
+} room_t;
+
+static room_t newRoom() {
+	room_t r;
+	r.roomNumber = randomRoom();
+	r.callTime = 10;
+	return r;
+}
+
+int randomRoom() {
+	//srand(time(NULL));
+
+	return rand_r() % MAXROOM;
+}
+
+static void showRoom(room_t r) {
+	printf("Room Number: %d, Call Time: %d\n", r.roomNumber, r.callTime);
+}
+
+static int getRandomSleep() {
+	//srand(time(NULL));
+
+	return rand_r() % SLEEP;
+}
+
+static void randomSleep() {
+	int randomNumber = getRandomSleep();
+
+	printf("%d\n", randomNumber);
+
+	sleep(randomNumber);
+}
+
+static void * generateCall(void *log_in) {
+	//srand(time(NULL));
+	logs_t *log = log_in;
+	while(1) {
+		randomSleep();
+
+		room_t room = newRoom();
+
+		showRoom(room);
+	}
+}
 
 int main() {
-	while(1) {
-		//int roomNumber = randomRoom();
-		//Registering: RoomNumber Day Month Date Time Year
-		//printf("Registering: %d\n", roomNumber);
-	}
+	logs_t log;
+	srand(time(NULL));
+
+	pthread_t genCall_t;
+
+	pthread_create(&genCall_t, NULL, &generateCall, (void *)&log);
+
+	pthread_join(genCall_t, NULL);
+
+	//int roomNumber = randomRoom();
+	//Registering: RoomNumber Day Month Date Time Year
+	//printf("Registering: %d\n", roomNumber);
 }
