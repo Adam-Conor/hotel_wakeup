c4114a31e4614c1334434dd22e07d50f5bb71203
Conor Smyth <cnrsmyth@gmail.com>
2014-12-01 18:20:02 +0000

    Got nodes working backwards

--- hotel.c
+++ hotel.c
@@ -11,15 +11,19 @@
 #include <unistd.h>
 #include <stdbool.h>
 
+/* Declare constants */
+
 #define SLEEP 5
 #define MAXROOM 8000
+
+/* define all structs */
 
 typedef struct { //stuct for wake up time
 	int roomNumber;
 	time_t callTime;
 } wakeupCall_t;
 
-typedef struct { //stuct for nodes in heap
+typedef struct Node { //stuct for nodes in heap
 	wakeupCall_t time;
 	struct Node *next;
 } Node;
@@ -31,40 +35,81 @@ typedef struct { //stuct for holding logs
 	int expired;
 } logs_t;
 
+typedef struct { //struct for holding shared data
+	logs_t log;
+	Node *heap;
+} sharedData_t;
+
+/* Node methods */
+
+Node *add(Node *root, wakeupCall_t c) {
+	Node *newCall = (Node *) malloc(sizeof(Node));
+	newCall->time = c;
+	newCall->next = NULL;
+
+	if(root == NULL) {
+		return newCall;
+	}//if the heap empty
+
+	if(root->next == NULL) {
+		root->next = newCall;
+		return root;
+	}
+
+	/*if(newCall->time.callTime < root->time.callTime) {
+		newCall->next = root;
+		return newCall;
+	}//still not working properly
+	*/
+
+	Node *prev = root;
+	Node *current = root->next;
+
+	while(current->next != NULL && current->time.callTime > newCall->time.callTime) {
+		prev = current;
+		current = current->next;
+	}
+	
+	newCall->next = current;
+	prev->next = newCall;
+	//current->next = newCall;
+
+
+	return root;
+}//add a node to heap
+
+/* Add remove node to remove triggered alarms */
+
+/* */
+
+void showHeap(Node *root) {
+	Node *temp = root;
+
+	while(temp != NULL) {
+		printf("ShowHeap %04d %s\n", temp->time.roomNumber, ctime(&temp->time.callTime));
+		temp = temp->next;
+	}
+}
+
+/* Initialisation methods */
+
 void initLog(logs_t *log) {
 	log->generated = 0;
 	log->pending = 0;
 	log->called = 0;
 	log->expired = 0;
-}//initialise logs
+}
 
-/*Node *add(Node *root, wakeupCall_t c) {
-	Node *newCall = (Node *) malloc(sizeof(Node));
-	newCall = root;
-	newCall->time = c;
+void initNode(Node *ned) {
+	ned = (Node *) malloc(sizeof(Node));
+}
 
-	if(root == NULL) {
-		newCall->next = NULL;
-		return newCall;
-	}//if the heap empty
+void initData(sharedData_t *data) {
+	initLog(&data->log);
+	initNode(data->heap);
+}
 
-	if(c.callTime < root->time.callTime) {
-		Node *diff  = (Node *) malloc(sizeof(Node));
-		diff = root;
-		newCall->next = diff;
-		return newCall;
-	}//if 
-
-	root = add(root->next, c);
-}//add a node to heap
-*/
-
-static wakeupCall_t newCall() {
-	wakeupCall_t c;
-	c.roomNumber = randomRoom();
-	c.callTime = randomCall();
-	return c;
-}//generate a new wake up call
+/* Random number generators */
 
 static int getRandomNumber(int x) {
 	unsigned int seedp = time(NULL);
@@ -87,11 +132,6 @@ int randomCall() {
 	return getRandomCall();
 }//get a random call time
 
-static void showCall(wakeupCall_t c) {
-	//needs to add to data structure
-	printf("Registering:\t%04d %s\n", c.roomNumber, ctime(&c.callTime));
-}
-
 static int getRandomSleep() {
 	return getRandomNumber(SLEEP);
 }//returns random sleep time
@@ -100,32 +140,58 @@ static void randomSleep() {
 	sleep(getRandomSleep());
 }//sleep for a random time
 
-/*
- * at the moment just generates random wake up calls at random times
- */
+/* wakeupCall methods */
 
-static void * generateCall(void *log_in) {
-	logs_t *log = log_in;
+static wakeupCall_t newCall() {
+  wakeupCall_t c;
+  c.roomNumber = randomRoom();
+  c.callTime = randomCall();
+  return c;
+}//generate a new wake up call
+
+static void showCall(wakeupCall_t c) {
+	//needs to add to data structure
+	printf("Registering:\t%04d %s\n", c.roomNumber, ctime(&c.callTime));
+}
+
+/* log methods */
+
+void log_new(logs_t *log) {
+	log->generated++;
+}
+
+/* at the moment just generates random wake up calls at random times */
+
+static void * generateCall(void *data_in) {
+	sharedData_t *data = data_in;
 
 	while(1) {
+		//sleep for random seconds
 		randomSleep();
 
+		//generate a wake up call
 		wakeupCall_t call = newCall();
-
 		showCall(call);
-		log->generated++;
-		printf("%d\n", log->generated);
+
+		//add the call to the heap
+		data->heap = add(data->heap, call);
+		showHeap(data->heap);
+
+		//log the new call
+		log_new(&data->log);
+		printf("%d\n", data->log.generated);
 	}
 }
 
 int main() {
-	logs_t log;
+	//delcare shared data
+	sharedData_t data;
 
-	initLog(&log);
+	initData(&data);
 
 	pthread_t genCall_t;
 
-	pthread_create(&genCall_t, NULL, &generateCall, (void *)&log);
+	pthread_create(&genCall_t, NULL, &generateCall, (void *)&data);
 
 	pthread_join(genCall_t, NULL);
 }
